0837543edb84ad62da5cb866a76489f1a68151f1
Coding <m64513824@gmail.com>
2025-08-22 16:33:26 +0300

    Creating the Task class

---
+++ headers/task.h
@@ -0,0 +1,20 @@
+#ifndef TASK_H
+#define TASK_H
+#include <iostream>
+
+class Task {
+    private:
+        int id;
+        std::string description;
+        bool isCompleted;
+    public:
+        Task();
+        Task(int id);
+
+        void setID(int id);
+        int getID() const;
+        std::string getDescription() const;
+        std::string getStatus() const;
+};
+
+#endif // TASK_H
